7c41060ea91791889e24c173a17a3e8995f0ca7d
Kevin Allen <kevjallen16@gmail.com>
2022-03-03 17:56:03 -0500

    fix: simulate merge during integration (#3)

--- _internals/aws-stack/bin/integration.ts
+++ _internals/aws-stack/bin/integration.ts
@@ -13,14 +13,12 @@ const integrationSiteProps: StaticSiteStackProps = {
 export default integrationSiteProps;
 
 const app = new cdk.App();
-
 const cdkProjectPath = '_internals/aws-stack';
 
 new IntegrationStack(app, 'StaticSiteIntegrationPipeline', {
   buildImageFromEcr: 'ubuntu-build:v1.1.2',
   cleanUpCommands: [
     `cd ${cdkProjectPath}`,
-    'npm install',
     'npm run cdk destroy -- --force --app="$INTEGRATION_SITE_APP"'
       + ' -c subdomain="$CODEBUILD_RESOLVED_SOURCE_VERSION"',
   ],
@@ -29,14 +27,22 @@ new IntegrationStack(app, 'StaticSiteIntegrationPipeline', {
     '. $ASDF_SCRIPT && asdf install',
   ],
   integrationCommands: [
+    // user details for simulated merge
+    'git config --global user.name "codebuild"',
+    `git config --global user.email "codebuild@${integrationSiteProps.domainName}"`,
+    // simulate merge into trunk branch
+    'git checkout $CODEBUILD_WEBHOOK_BASE_REF',
+    'git merge --no-commit --no-ff $CODEBUILD_RESOLVED_SOURCE_VERSION',
+    // build site
     'bundle install',
     'bundle exec jekyll build',
+    // CDK integration
     `cd ${cdkProjectPath}`,
     'npm install',
     'npm run lint',
     'npm run test',
-    'npm run cdk synth -- --app="$INTEGRATION_SITE_APP"'
-      + ' -c subdomain="$CODEBUILD_RESOLVED_SOURCE_VERSION"',
+    'npm run cdk synth',
+    // deploy and test live integration site
     'npm run cdk deploy -- --app="$INTEGRATION_SITE_APP"'
       + ' -c subdomain="$CODEBUILD_RESOLVED_SOURCE_VERSION"',
     'curl -sf "https://$CODEBUILD_RESOLVED_SOURCE_VERSION'
